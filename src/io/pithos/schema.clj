59b150f1bab5997bdc2f39be90d47734adfedc06
Mitch Williams <miwilliams@vmware.com>
2016-08-29 11:18:10 -0700

    Make schema installation more idempotent

--- src/io/pithos/schema.clj
+++ src/io/pithos/schema.clj
@@ -1,27 +1,30 @@
 (ns io.pithos.schema
   "Namespace holding a single action which installs the schema"
   (:require [clojure.tools.logging :refer [info error]]
+            [clojure.string        :as string]
             [io.pithos.system      :as system]
             [io.pithos.store       :as store]))
+
+(defn converge-schema-part [desc schema-part]
+  (try
+    (info (string/join ["converging " desc "..."]))
+    (store/converge! schema-part)
+  (catch clojure.lang.ExceptionInfo e
+    (if (= com.datastax.driver.core.exceptions.AlreadyExistsException (class (get (ex-data e) :exception)))
+      (info desc "already exists, continuing.")
+      (throw e)))))
 
 (defn converge-schema
   "Loops through all storage layers and calls converge! on them"
   ([system exit?]
      (info "converging all schemas...")
      (try
-       (info "converging bucketstore schema")
-       (store/converge! (system/bucketstore system))
-
+       (converge-schema-part "bucketstore schema" (system/bucketstore system))
        (doseq [region (system/regions system)
                :let [[region {:keys [metastore storage-classes]}] region]]
-         (info "converging metastore for region " region)
-         (store/converge! metastore)
-
+         (converge-schema-part (string/join " " ["metastore for" region]) metastore)
          (doseq [[storage-class blobstore] storage-classes]
-           (info "converging blobstore for region and storage-class "
-                 region storage-class)
-           (store/converge! blobstore)))
-
+           (converge-schema-part (string/join " " ["blobstore for region and storage-class" region storage-class]) blobstore)))
        (catch Exception e
          (error e "cannot create schema"))
        (finally
